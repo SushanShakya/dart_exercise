f828627a300a60ba27e4b541ac3423e0510c1037
SushanShakya <sushaanshakya88@gmail.com>
2024-09-03 18:14:41 +0545

    Feat: Variables Exercise

---
+++ 1_variables.dart
@@ -0,0 +1,17 @@
+class Variables {
+  int add(int a, int b) {
+    // Write Code here
+  }
+
+  int subtract(int a, int b) {
+    // Write Code here
+  }
+
+  int addAndMultiply(int a, int b) {
+    // Write Code here
+  }
+
+  int subtractAndDivide(int a, int b) {
+    // Write Code Here
+  }
+}
