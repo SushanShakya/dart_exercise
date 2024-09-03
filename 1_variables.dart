b7069cb56affa5e2c7fcf666965abf97981ce668
SushanShakya <sushaanshakya88@gmail.com>
2024-09-03 18:55:26 +0545

    Feat: New Questions

--- 1_variables.dart
+++ 1_variables.dart
@@ -2,19 +2,76 @@ import '1_variables_test.dart';
 
 class Variables {
   int add(int a, int b) {
-    // Write Code here
+    return a + b;
   }
 
   int subtract(int a, int b) {
-    // Write Code here
+    return a - b;
   }
 
-  int addAndMultiply(int a, int b) {
-    // Write Code here
+  int addAndMultiply(int a, int b, int c) {
+    // Write code to add a & b
+    // Multiply result with c
+    /**
+     * int a = 10; 
+     * a = 2;
+     */
+    int d = a + b;
+    return d * c;
   }
 
-  int subtractAndDivide(int a, int b) {
-    // Write Code Here
+  int subtractAndDivide(int a, int b, int c) {
+    // Write code to add a & b
+    // Divide the result by c
+    int d = a - b;
+    return d ~/ c;
+  }
+
+  double addMultiplyDivide(int a, int b) {
+    // Add a & b
+    // Multiply the result by a
+    // Divide the result by b
+    d = a 
+  }
+
+  int isEven(int a) {
+    // Write code to check if a is even or not
+  }
+
+  int isOdd(int a) {
+    // Write code to check if a is odd or not
+  }
+
+  bool checkBothTrue(bool a, bool b) {
+    // Write code to check if both a & b are true
+  }
+
+  bool checkBothFalse(bool a, bool b) {
+    // Write code to check if both a & b are false
+  }
+
+  bool checkOneTrue(bool a, bool b) {
+    // Write code to check if either a or b is true
+  }
+
+  bool checkOneFalse(bool a, bool b) {
+    // Write code to check if either a or b is false
+  }
+
+  bool checkGreaterThan(int a, int b) {
+    // Check if a is greater than b
+  }
+
+  bool checkLessThan(int a, int b) {
+    // Check if a is less than b
+  }
+
+  bool checkLessThanEqual(int a, int b) {
+    // Check if b is less than or equal to a
+  }
+
+  bool checkGreaterThanEqual(int a, int b) {
+    // Check if b is greater than or equal to a
   }
 }
 
--- 1_variables_test.dart
+++ 1_variables_test.dart
@@ -1,60 +1,60 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import '1_variables.dart';
+// // ignore_for_file: public_member_api_docs, sort_constructors_first
+// import '1_variables.dart';
 
-class VariablesTest {
-  final Variables solution;
-  VariablesTest({
-    required this.solution,
-  });
+// class VariablesTest {
+//   final Variables solution;
+//   VariablesTest({
+//     required this.solution,
+//   });
 
-  void testSolution(testCases, int Function(int, int) fn, fnName) {
-    for (var e in testCases) {
-      var res = fn(e[0], e[1]);
-      if (e[2] == res) {
-        continue;
-      }
-      print("Inputs : ${e[0]}, ${e[1]}");
-      print("Expected Output : ${e[2]}");
-      print("Actual Output : ${res}");
-      print("${fnName}() is incorrect.");
-      return;
-    }
-    print("${fnName}() is correct.");
-  }
+//   void testSolution(testCases, int Function(int, int) fn, fnName) {
+//     for (var e in testCases) {
+//       var res = fn(e[0], e[1]);
+//       if (e[2] == res) {
+//         continue;
+//       }
+//       print("Inputs : ${e[0]}, ${e[1]}");
+//       print("Expected Output : ${e[2]}");
+//       print("Actual Output : ${res}");
+//       print("${fnName}() is incorrect.");
+//       return;
+//     }
+//     print("${fnName}() is correct.");
+//   }
 
-  void testAdd() {
-    var testCases = [
-      [10, 20, 30],
-      [-1, 10, 9],
-      [-1, -2, -3],
-      [1, -2, -1],
-    ];
-    testSolution(testCases, solution.add, "add");
-  }
+//   void testAdd() {
+//     var testCases = [
+//       [10, 20, 30],
+//       [-1, 10, 9],
+//       [-1, -2, -3],
+//       [1, -2, -1],
+//     ];
+//     testSolution(testCases, solution.add, "add");
+//   }
 
-  void testAddAndMultiply() {
-    var testCases = [
-      [10, 20, ((10 + 20) * 10 * 20)],
-      [-1, 20, (((-1) + 20) * (-1) * 20)],
-      [-10, -20, (((-10) + (-20)) * (-10) * (-20))],
-      [10, -20, ((10 + (-20)) * 10 * 20)],
-    ];
-    testSolution(testCases, solution.addAndMultiply, "addAndMultiply");
-  }
+//   void testAddAndMultiply() {
+//     var testCases = [
+//       [10, 20, ((10 + 20) * 10 * 20)],
+//       [-1, 20, (((-1) + 20) * (-1) * 20)],
+//       [-10, -20, (((-10) + (-20)) * (-10) * (-20))],
+//       [10, -20, ((10 + (-20)) * 10 * 20)],
+//     ];
+//     testSolution(testCases, solution.addAndMultiply, "addAndMultiply");
+//   }
 
-  void testSubtractAndDivide() {
-    var testCases = [
-      [10, 20, ((10 + 20) / 10 / 20)],
-      [-1, 20, (((-1) + 20) / (-1) / 20)],
-      [-10, -20, (((-10) + (-20)) / (-10) / (-20))],
-      [10, -20, ((10 + (-20)) / 10 / 20)],
-    ];
-    testSolution(testCases, solution.addAndMultiply, "addAndMultiply");
-  }
+//   void testSubtractAndDivide() {
+//     var testCases = [
+//       [10, 20, ((10 + 20) / 10 / 20)],
+//       [-1, 20, (((-1) + 20) / (-1) / 20)],
+//       [-10, -20, (((-10) + (-20)) / (-10) / (-20))],
+//       [10, -20, ((10 + (-20)) / 10 / 20)],
+//     ];
+//     testSolution(testCases, solution.addAndMultiply, "addAndMultiply");
+//   }
 
-  void runAll() {
-    testAdd();
-    testAddAndMultiply();
-    testSubtractAndDivide();
-  }
-}
+//   void runAll() {
+//     testAdd();
+//     testAddAndMultiply();
+//     testSubtractAndDivide();
+//   }
+// }
